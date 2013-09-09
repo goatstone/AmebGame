0ca3e0c711219f0b614ea786a612ccf16edd84b0
goatstone <goatstone@gmail.com>
2013-09-09 11:11:51 -0700

    refactor of game.js and related files

--- js/Collide.js
+++ js/Collide.js
@@ -5,6 +5,7 @@
 
 var Collide = function () {
 
+    //Collide.trianglePoint(Triangle ta, Point p);
     function trianglePoint(ta, dot) {
 
         var alpha = ((ta.p2.y - ta.p3.y) * (dot.x - ta.p3.x) + (ta.p3.x - ta.p2.x) * (dot.y - ta.p3.y)) /
@@ -18,8 +19,11 @@ var Collide = function () {
         return (alpha > 0 && beta > 0 && gamma > 0);
     }
 
+    //Collide.circlePoint(Circle c, Point p);
     function circlePoint(c, p) {
+
         var distR = Math.sqrt(Math.pow(p.x - c.pos.x, 2) + Math.pow(p.y - c.pos.y, 2))
+
         return distR < c.size;
     }
 
